517757f7597323d9003031d3169a55a85fdd9c54
mohanrajaddluru <addluru.m@northeastern.edu>
2023-10-06 16:48:10 -0400

    updating workflow

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ const userExist = async (id) => {
 }
 
 function startServer() {
-    const port = process.env.PORT || 8081;
+    const port = process.env.PORT || 8080;
     app.listen(port, () => {
         console.log(`Server is running on port ${port}`);
     });
