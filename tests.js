0a59fb3742ba018c92eb869553e8e4b104c344db
mohanrajaddluru <addluru.m@northeastern.edu>
2023-10-12 16:38:46 -0400

    Updated errors and workflows Updated 2.1

--- tests.js
+++ tests.js
@@ -16,28 +16,5 @@ describe('Testing our Application', function () {
 
         assert.equal(1, 1);
 
-    });
-
- 
-
-    it('GET /healthz end point of the application', (done) => {
-
-        supertest(app)
-
-            .get('/healthz')
-
-            .expect(200)
-
-            .end((err, response) => {
-
-                if (err) return done(err)
-
-                return done()
-
-            })
-
     })
-
-    
-
 });
