41c488059becfce24af72ceea1d29a15130e7767
mohanrajaddluru <addluru.m@northeastern.edu>
2023-10-17 18:49:47 -0400

    final test

--- database.js
+++ database.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const sequelize = new Sequelize({
     database: process.env.DATABASE_NAME, 
     username: process.env.DATABASE_USER, 
+    password: null,
     host: process.env.DATABASE_HOST,
     dialect: 'mysql', // Change this based on your database
 });
