0c4563d01516f8c6d3358bb097a074ea452bab81
mohanrajaddluru <addluru.m@northeastern.edu>
2023-10-17 08:43:12 -0400

    added password

--- database.js
+++ database.js
@@ -1,12 +1,6 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-console.log('DATABASE_NAME:', process.env.DATABASE_NAME);
-console.log('DATABASE_USER:', process.env.DATABASE_USER);
-console.log('DATABASE_PASSWORD:', process.env.DATABASE_PASSWORD);
-console.log('DATABASE_HOST:', process.env.DATABASE_HOST);
-
-
 const sequelize = new Sequelize({
     database: process.env.DATABASE_NAME, 
     username: process.env.DATABASE_USER, 
