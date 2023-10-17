07312ee2199885af7e051e33f04bb93fd936447d
mohanrajaddluru <addluru.m@northeastern.edu>
2023-10-17 12:27:54 -0400

    added required debug logs

--- install.sh
+++ install.sh
@@ -1,13 +1,30 @@
 sleep 30
-sudo apt-get update
-sudo apt-get install -y mariadb-server unzip curl
-curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
-sudo apt-get install -y nodejs npm
+sudo apt update
+sudo apt install nodejs
+sudo node -v
+sudo apt install npm
+sudo apt install curl
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt install nodejs
 node -v
+npm -v
+
+echo "MohanLog1"
+
+sudo apt-get install mariadb-server unzip -y
+
 sudo systemctl start mysql
 sudo systemctl enable mysql
 sudo systemctl status mysql
 sudo apt-get install expect
+
+echo "MohanLog2"
+
+sudo pwd
+
+echo "MohanLog3"
+
+sudo ls -ltrh
 
 expect <<EOF
 set timeout -1
@@ -35,11 +52,26 @@ expect eof
 EOF
 
 #sudo mysql -u root -e "CREATE USER 'test'@'127.0.0.1' IDENTIFIED BY ' '; GRANT CREATE, ALTER, DROP, INDEX, INSERT, SELECT, UPDATE, DELETE, CREATE TEMPORARY TABLES, LOCK TABLES ON *.* TO 'test'@'127.0.0.1'; GRANT ALL PRIVILEGES ON *.* TO 'test'@'127.0.0.1'; FLUSH PRIVILEGES;",
+
+
 sudo mysql -u root -e "CREATE DATABASE test;"
+
+echo "created test database"
+
 sudo mysql -u root -e "SHOW DATABASES";
 
+echo "list of databases"
+
+sudo pwd
+
+echo "below are current repo folders"
+
+sudo ls -ltrh
+
+
+
 cd ~/ && unzip webapp.zip
-cd ~/webapp && npm i
+#cd ~/webapp
 
 
 sudo mv /tmp/webapp.service /etc/systemd/system/webapp.service
