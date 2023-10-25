7bae55c2d8a5422ce857690c9d7871ebf4582014
mohanrajaddluru <addluru.m@northeastern.edu>
2023-10-25 02:58:14 -0400

    added review comments and security

--- install.sh
+++ install.sh
@@ -16,62 +16,6 @@ sudo useradd -m -p $(openssl passwd -1 password) webappuser
 sudo cat /etc/passwd
 
 
-#npm ci
-
-echo "MohanLog1"
-
-#sudo apt-get install mariadb-server unzip -y
-
-#sudo systemctl start mysql
-#sudo systemctl enable mysql
-#sudo systemctl status mysql
-#sudo apt-get install expect
-
-echo "MohanLog2"
-
-sudo pwd
-
-echo "MohanLog3"
-
-sudo ls -ltrh
-
-# expect <<EOF
-# set timeout -1
-# spawn sudo mysql_secure_installation
-
-# expect "Enter current password for root (enter for none):"
-# send "\r"
-
-# expect "Set root password? [Y/n]"
-# send "n\r"
-
-# expect "Remove anonymous users? [Y/n]"
-# send "n\r"
-
-# expect "Disallow root login remotely? [Y/n]"
-# send "n\r"
-
-# expect "Remove test database and access to it? [Y/n]"
-# send "n\r"
-
-# expect "Reload privilege tables now? [Y/n]"
-# send "Y\r"
-
-# expect eof
-# EOF
-
-# # sudo mysql -u root -e "create user mohan identified by 'password'"
-# # sudo mysql -u root -e "create database test"
-# # sudo mysql -u root -e "grant all previliges on test.* to 'mohan'@'localhost' identified by 'password'"
-
-# sudo mysql -u root -e "create user 'mohan'@'localhost' identified by 'password'"
-# sudo mysql -u root -e "create database test"
-# sudo mysql -u root -e "grant all privileges on test.* to 'mohan'@'localhost' identified by 'password'"
-# #npm install --save
-# #npm fund
-
-# echo "list of databases"
-
 sudo pwd
 
 echo "below are current repo folders"
@@ -86,13 +30,8 @@ ls -ltrah /home/webappuser/webapp/
 
 cd /home/webappuser/webapp
 
-#mkdir webapp
 
 ls -ltrah
-
-#cp webapp.zip webapp/
-
-#cd webapp/
 
 pwd
 
@@ -106,25 +45,14 @@ sudo npm install --save
 
 sudo npm fund
 
-# echo "DATABASE_HOST: mohan.c4tltzid5dl3.us-east-1.rds.amazonaws.com" >> ~/webapp/.env
-
-# echo "DATABASE_USER: mohan" >> ~/webapp/.env
-
-# echo "DATABASE_NAME: test" >> ~/webapp/.env
-
-# echo "DATABASE_PASSWORD: password" >> ~/webapp/.env
-
-# echo "PORT: 8080" >> ~/webapp/.env
-
 ls -ltrah
 
-#cat ~/webapp/.env
 
 cd /home/admin/
 
-sudo chown -R webapp:webapp /home/webapp/webapp 
+sudo chown -R webappuser:webappuser /home/webappuser/webapp 
 
-sudo ls -ltrah /home/webapp/
+sudo ls -ltrah /home/webappuser/
 
 sudo mv /tmp/webapp.service /etc/systemd/system/webapp.service
 
