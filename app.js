5767ca8ab5d28cca38e49f4017a680c5ebf95d11
mohanrajaddluru <addluru.m@northeastern.edu>
2023-11-04 22:45:06 -0400

    added log files

--- app.js
+++ app.js
@@ -5,12 +5,37 @@ const { parse } = require('csv-parse');
 const bcrypt = require('bcrypt');
 const {uuid} = require('uuidv4');
 const bodyParser = require('body-parser');
+
+//const logger = require('pino')();
+const winston = require('winston');
+
 const { UserSchema, AssignmentSchema} = require('./models/index.js');
 const assignmentRoute = require('./routes/assignment.js');
 const { processCSVFile } = require('./userCreate.js')
 const  healthCheckRoutes  = require('./routes/healthCheck.js');
 
 const { bootstrapDatabase } = require('./util/bootstrapdb.js');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(), // Add a timestamp to log entries
+    winston.format.simple()
+  ),
+  transports: [
+    new winston.transports.Console(), // Log to the console
+    new winston.transports.File({ filename: './logs/all-logs.log'}),
+    new winston.transports.File({ filename: './logs/error.log', level: 'error' }), // Log errors to a file
+    new winston.transports.File({ filename: './logs/info.log', level: 'info' }), // Log info to a file
+    new winston.transports.File({ filename: './logs/warn.log', level: 'warn' }) // Log warn to a file
+  ],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+      format: winston.format.simple(),
+    }));
+  }
 
 
 const app = express();
@@ -77,20 +102,22 @@ function startServer() {
 
 async function main() {
     try {
-      // Bootstrap the database
       if (await bootstrapDatabase())
       {
         processCSVFile();
         startServer();
         console.log("success")
+        logger.info('Application Started')
       }
       else
       {
         startServer();
         console.log("failure");
+        logger.error(`Failed to connect Database, Application started and running at port ${process.env.PORT}`)
       }
     } catch (error) {
       console.error('Error in main:', error);
+      logger.error(`Failed to start application on`)
     }
   }
 
