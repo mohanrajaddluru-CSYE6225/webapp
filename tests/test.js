1110d65f97e7cfa8e21272c2bd327eb599c08514
mohanrajaddluru <addluru.m@northeastern.edu>
2023-11-05 20:20:33 -0500

    resolve conflicts

--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,5 @@
 const request = require('supertest')
-const apps = require('./app.js');
+const {app, logger} = require('../app/app.js');
 const chai = require('chai')
 const expect = chai.expect
 
@@ -8,7 +8,7 @@ const expect = chai.expect
 
     describe('Successes', function() {
         it('healthCheck to database ', function(done) {
-            request(apps).get('/healthz').send({ }).end(function(err, res) {
+            request(app).get('/healthz').send({ }).end(function(err, res) {
                 expect(res.statusCode).to.be.equal(200)
                 done()
             })
