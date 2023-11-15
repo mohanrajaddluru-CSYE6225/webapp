8b5e110d80653761b451604fd4fae3b174378cd3
Mohan Raj Addluru <113074818+mohanrajaddluru@users.noreply.github.com>
2023-11-15 09:45:57 -0500

    exposed metadata endpoint and instance id (#126)
    * exposed metadata endpoint
    
    * modified result declaration
    
    * exposed metadata endpoint and instance id

--- app/routes/latestmetadata.js
+++ app/routes/latestmetadata.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const fetchlatestmetadata = require('../controller/metadata.js');
 
-router.get('/', fetchlatestmetadata);
+router.get('/*', fetchlatestmetadata);
+
 
 module.exports = router;
